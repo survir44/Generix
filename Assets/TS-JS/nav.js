3ab4541f51c1506bbdd3bf374f074ea7c22d8255
survir44 <survir44@gmail.com>
2019-09-24 22:03:31 +0530

    Change Password

--- Assets/TS-JS/nav.js
+++ Assets/TS-JS/nav.js
@@ -1,8 +1,8 @@
 document.getElementById('login__button').addEventListener('click', function () {
-    location.href = "/home/winston/Desktop/Generix/Generix/login.html";
+    location.href = "login.html";
 });
 document.getElementById("register__button").addEventListener('click', function () {
-    location.href = "/home/winston/Desktop/Generix/Generix/register.html";
+    location.href = "register.html";
 });
 var header = document.getElementById("header__container");
 window.onscroll = function () {
